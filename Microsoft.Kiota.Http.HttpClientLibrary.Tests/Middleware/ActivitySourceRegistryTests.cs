f99c5fb5a3bdc9e914925ac65d80c82470ce43a4
Andrew Omondi <anomondi@microsoft.com>
2023-11-10 12:34:09 +0300

    Adds tests

---
+++ Microsoft.Kiota.Http.HttpClientLibrary.Tests/Middleware/ActivitySourceRegistryTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Kiota.Http.HttpClientLibrary.Middleware;
+using Xunit;
+
+namespace Microsoft.Kiota.Http.HttpClientLibrary.Tests.Middleware.Registries
+{
+    public class ActivitySourceRegistryTests
+    {
+        [Fact]
+        public void Defensive()
+        {
+            Assert.Throws<ArgumentNullException>(() => ActivitySourceRegistry.DefaultInstance.GetOrCreateActivitySource(""));
+            Assert.Throws<ArgumentNullException>(() => ActivitySourceRegistry.DefaultInstance.GetOrCreateActivitySource(null));
+        }
+
+        [Fact]
+        public void CreatesNewInstanceOnFirstCallAndReturnsSameInstance()
+        {
+            // Act
+            var activitySource = ActivitySourceRegistry.DefaultInstance.GetOrCreateActivitySource("sample source");
+            Assert.NotNull(activitySource);
+
+            var activitySource2 = ActivitySourceRegistry.DefaultInstance.GetOrCreateActivitySource("sample source");
+            Assert.NotNull(activitySource);
+
+            // They are the same instance
+            Assert.Equal(activitySource, activitySource2);
+        }
+
+        [Fact]
+        public void CreatesDifferentInstances()
+        {
+            // Act
+            var activitySource = ActivitySourceRegistry.DefaultInstance.GetOrCreateActivitySource("sample source");
+            Assert.NotNull(activitySource);
+
+            var activitySource2 = ActivitySourceRegistry.DefaultInstance.GetOrCreateActivitySource("sample source 2");
+            Assert.NotNull(activitySource);
+
+            // They are not the same instance
+            Assert.NotEqual(activitySource, activitySource2);
+        }
+    }
+}
